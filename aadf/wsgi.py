d5f213c6d22b1b6a09c1711c1fa71c792564bbf1
abc <aaa@qq.com>
2020-03-21 08:57:35 +0800

    第4

--- aadf/wsgi.py
+++ aadf/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aadf.settings")
 
 application = get_wsgi_application()
+
+888888888888888888
+9999999999999999999
