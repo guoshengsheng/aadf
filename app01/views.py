e4d32af028178590ca7f83c253a87ade5a2076d7
abc <aaa@qq.com>
2020-03-21 16:48:52 +0800

    8

--- app01/views.py
+++ app01/views.py
@@ -5,18 +5,4 @@ def Users(request123):
     obj_list=models.User.objects.all().select_related()
     for i in obj_list:
         print(i.name,i.department.title,i.role.title)
-        000
-        000
-        45654645
-        4545454
-        121121
-        455454
-        45554
-        1111111111
     return HttpResponse("OK")
-1111111111
-000000
-222222
-
-
-
