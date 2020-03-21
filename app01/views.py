eb1d5093b632919d62d7d6eeee9fd4fd87a8f737
abc <aaa@qq.com>
2020-03-21 18:21:52 +0800

    5

--- app01/views.py
+++ app01/views.py
@@ -5,6 +5,7 @@ def Users(request123):
     obj_list=models.User.objects.all().select_related()
     for i in obj_list:
         print(i.name,i.department.title,i.role.title)
+        master开发完毕
     return HttpResponse("OK")
 
 
