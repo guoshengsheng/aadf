6647b35753790547c55d848a27532355333a80ad
abc <aaa@qq.com>
2020-03-21 12:01:47 +0800

    提交信息（尽量详细）

--- app01/views.py
+++ app01/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse
 from app01 import models
 # Create your views here.
 def Users(request123):
+    456789
     obj_list=models.User.objects.all().select_related()
     for i in obj_list:
         print(i.name,i.department.title,i.role.title)
